b1eb8f3a942f51dcdc5dcf4d7c2e148a925951f0
eineboek <75323803+eineboek@users.noreply.github.com>
2021-02-14 16:21:58 +0100

    Projektdateien hinzufügen.

---
+++ PureHistory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PureHistory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
