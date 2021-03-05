886246fa5b7730b0a5a5249e3e80df3efe9fc3f3
eineboek <75323803+eineboek@users.noreply.github.com>
2021-03-05 20:26:58 +0100

    Updated Version strings for Release

--- src/PureHistory/Resources.Designer.cs
+++ src/PureHistory/Resources.Designer.cs
@@ -403,7 +403,7 @@ namespace PureHistory {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die PureHistory Version 1.0.1 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die PureHistory Version 1.1.0 ähnelt.
         /// </summary>
         internal static string ModVersion {
             get {
